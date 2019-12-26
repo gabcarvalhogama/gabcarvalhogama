2ba63bd90a32cfc021966ddb7614ebda549c57f8
Gabriel Carvalho Gama <gabriel.carvalhogama@hotmail.com>
2019-12-26 15:16:11 -0300

    Single page adjusts

--- single.php
+++ single.php
@@ -4,13 +4,6 @@
 	<div class="single--thumb">
 		<img src="<?=( has_post_thumbnail() ) ? get_the_post_thumbnail_url() : bloginfo('template_url').'/assets/images/post-placeholder.png';?>" alt="Imagem de destaque de: <?=the_title()?>">
 	</div>
-	<div class="single--share">
-		<ul class="default-ul">
-			<li><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?=get_permalink()?>"><i class="fab fa-facebook-f"></i></a></li>
-			<li><a target="_blank" href="http://twitter.com/share?text=Ei, vejam este artigo: <?=the_title();?>&url=<?=get_permalink()?>"><i class="fab fa-twitter"></i></a></li>
-			<li><a target="_blank" href="https://linkedin.com/"><i class="fab fa-linkedin"></i></a></li>
-		</ul>
-	</div>
 	<div class="single--post">
 		<h1><?php the_title(); ?></h1>
 		<div class="single--post-meta">
@@ -29,19 +22,21 @@
 	 		?>
 		</article>
 
+		<div class="single--post-share">
+			<ul class="default-ul">
+				<li><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?=get_permalink()?>"><i class="fab fa-facebook-f"></i></a></li>
+				<li><a target="_blank" href="http://twitter.com/share?text=Ei, vejam este artigo: <?=the_title();?>&url=<?=get_permalink()?>"><i class="fab fa-twitter"></i></a></li>
+				<li><a target="_blank" href="https://linkedin.com/"><i class="fab fa-linkedin"></i></a></li>
+			</ul>
+		</div>
+
 		<div class="single--post-comments">
 			<div id="disqus_thread"></div>
 			<script>
-
-			/**
-			*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-			*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
-			
 			var disqus_config = function () {
 				this.page.url = "<?=the_permalink()?>";  // Replace PAGE_URL with your page's canonical URL variable
 				this.page.identifier = "<?=the_permalink()?>";
 			};
-			
 			(function() { // DON'T EDIT BELOW THIS LINE
 			var d = document, s = d.createElement('script');
 			s.src = 'https://gabriel-gama-dev.disqus.com/embed.js';
@@ -49,10 +44,10 @@
 			(d.head || d.body).appendChild(s);
 			})();
 			</script>
-			<noscript>Por favor, ative o JavaScript para ver <a href="https://disqus.com/?ref_noscript">os comentários.</a></noscript>
-			                            
+			<noscript>Por favor, ative o JavaScript para ver <a href="https://disqus.com/?ref_noscript">os comentários.</a></noscript>                          
 		</div>
 	</div>
+
 </main>
 
 <?php get_footer(); ?>
